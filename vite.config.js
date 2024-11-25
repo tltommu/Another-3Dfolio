b63124664d5c058f81a3cb99d9393d474e213b39
tltommu <tltommu527@gmail.com>
2024-11-26 00:34:53 +0800

    fixing email

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  
-})
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
 
+  return {
+    // Vite configuration
+    define: {
+      __VITE_EMAILJS_SERVICE_ID__: JSON.stringify(env.VITE_EMAILJS_SERVICE_ID),
+      __VITE_EMAILJS_TEMPLATE_ID__: JSON.stringify(env.VITE_EMAILJS_TEMPLATE_ID),
+      __VITE_EMAILJS_PUBLIC_KEY__: JSON.stringify(env.VITE_EMAILJS_PUBLIC_KEY),
+    },
+    plugins: [react()], // Include plugins here
+  };
+});
