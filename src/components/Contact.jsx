a6be134e502e063251160d13f8ba576a145fa7f9
tltommu <tltommu527@gmail.com>
2024-11-25 23:32:35 +0800

    fixing email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: "Tommy Lam",
           from_email: form.email,
-          to_email: "tommylam.ct@gmail.com",
+          to_email: "tltommu527@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
